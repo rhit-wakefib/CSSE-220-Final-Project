5ba4493b952b1c5a2c9b965131a2cc254f658f86
rhit-wakefib <wakefib@rose-hulman.edu>
2025-10-30 17:02:06 -0400

    Added the outline for the ground.

--- final/src/game/GameComponent.java
+++ final/src/game/GameComponent.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import javax.swing.JComponent;
 import javax.swing.Timer;
@@ -13,12 +14,14 @@ import javax.swing.Timer;
 public class GameComponent extends JComponent {
 
 
-	public static final int WIDTH = 400;
-	public static final int HEIGHT = 150;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 300;
 	public static final Color BG = Color.CYAN;
-	public static final Color FG = Color.BLACK;
+	public static final Color FG = Color.GREEN;
 	private int x1 = WIDTH/2, x2 = WIDTH/2;
 	private int y1 = HEIGHT/8, y2 = HEIGHT/8 *7;
+	private int groundStartX = 0, groundEndX = WIDTH;
+	private int groundY = HEIGHT - (HEIGHT/3);
 	private final EnemyModel model;
 	private final HudModel hud;
 
@@ -43,8 +46,16 @@ public class GameComponent extends JComponent {
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D) g;
+			
+			Rectangle r = new Rectangle(groundStartX,groundY, WIDTH, HEIGHT);
+			
 			g2.setColor(FG);
 			g2.drawLine(x1, y1, x2, y2);
+			
+			g2.draw(r);
+			g2.fill(r);
+			
+//			g2.drawLine(groundStartX, groundY, groundEndX, groundY);
 	        for (Enemy b : model.getBalls()) {
 	            b.draw(g2);
 	        }
