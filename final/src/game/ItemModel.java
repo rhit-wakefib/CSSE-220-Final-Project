cdd422aea19e0a95c470078841de6e0c8e3d5235
kofahlc <kofahlc@137.112.220.33>
2025-10-31 13:55:30 -0400

    added a class to handle collisions between player and item

--- final/src/game/ItemModel.java
+++ final/src/game/ItemModel.java
@@ -4,5 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemModel {
-	private List<Item> Items = new ArrayList<>();
+	private ArrayList<Item> Items = new ArrayList<>();
+	public void addItem(Item item) {
+		Items.add(item);
+	}
+	 private void handleCollisions(Player player) {
+		 for (int i = 0; i < Items.size(); i++) {
+			 Player a = player;
+			 Item b = Items.get(i);
+			 int dx = a.x - b.x, dy = a.y - b.y;
+	         int rSum = a.radius + b.radius;
+	         if (dx*dx + dy*dy <= rSum*rSum) {
+	        	 //Implement a method to keep track of some sort of score/coin value if the item being collided with is either a coin or a powerup
+	        	 
+	         }
+	 }
+}
 }
