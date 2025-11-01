77f8c40079b0a0a14ec53717558a75f01b1c66cf
rhit-wakefib <wakefib@rose-hulman.edu>
2025-11-01 13:23:07 -0400

    After merge cleanup

--- final/src/game/ItemModel.java
+++ final/src/game/ItemModel.java
@@ -8,16 +8,16 @@ public class ItemModel {
 	public void addItem(Item item) {
 		Items.add(item);
 	}
-	 private void handleCollisions(Player player) {
-		 for (int i = 0; i < Items.size(); i++) {
-			 Player a = player;
-			 Item b = Items.get(i);
-			 int dx = a.x - b.x, dy = a.y - b.y;
-	         int rSum = a.radius + b.radius;
-	         if (dx*dx + dy*dy <= rSum*rSum) {
-	        	 //Implement a method to keep track of some sort of score/coin value if the item being collided with is either a coin or a powerup
-	        	 
-	         }
-	 }
+//	 private void handleCollisions(Player player) {
+//		 for (int i = 0; i < Items.size(); i++) {
+//			 Player a = player;
+//			 Item b = Items.get(i);
+//			 int dx = a.x - b.x, dy = a.y - b.y;
+//	         int rSum = a.radius + b.radius;
+//	         if (dx*dx + dy*dy <= rSum*rSum) {
+//	        	 //Implement a method to keep track of some sort of score/coin value if the item being collided with is either a coin or a powerup
+//	        	 
+//	         }
+//	 }
 }
-}
+
